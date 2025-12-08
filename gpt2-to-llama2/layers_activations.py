e609932cac8dd699d60c9a4859bfa7094937cb6b
pichuu <mitulagarwal1800@gmail.com>
2025-12-08 21:09:54 +0530

    working on llama2

--- gpt2-to-llama2/layers_activations.py
+++ gpt2-to-llama2/layers_activations.py
@@ -21,6 +21,62 @@ class silu(nn.Module):
     def forward(self, x):
         return x * torch.sigmoid(x)
     
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_in, d_out, context_length, num_heads, dtype=None):
+        super().__init__()
+        assert d_out % num_heads == 0, "d_out must be divisible by num_heads"
+
+        self.d_in = d_in
+        self.num_heads = num_heads
+        self.head_dim = d_out // num_heads
+
+        self.W_query = nn.Linear(d_in, d_out, bias=False, dtype=dtype)
+        self.W_key = nn.Linear(d_in, d_out, bias=False, dtype=dtype)
+        self.W_value = nn.Linear(d_in, d_out, bias=False, dtype=dtype)
+
+        self.out_proj = nn.Linear(d_out, d_out, bias=False, dtype=dtype)
+
+        self.register_buffer(
+            "mask",
+            torch.triu(torch.ones(context_length, context_length), 
+            diagonal=1)
+        )
+
+        cos, sin = precompute_rope_params(head_dim=self.head_dim, context_length=context_length)
+        self.register_buffer("cos", cos)
+        self.register_buffer("sin", sin)
+
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape
+
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+
+        keys = keys.view(b, num_tokens, self.num_heads, self.head_dim)
+        queries = queries.view(b, num_tokens, self.num_heads, self.head_dim)
+        values = values.view(b, num_tokens, self.num_heads, self.head_dim)
+
+        keys = keys.transpose(1,2)
+        queries = queries.transpose(1,2)
+        values = values.transpose(1,2)
+
+        keys = compute_rope(keys, self.cos, self.sin)
+        queries = compute_rope(queries, self.cos, self.sin)
+
+        attn_scores = queries @ keys.T(2,3)
+        mask_bool = self.mask.bool()[:num_tokens, num_tokens:]
+        attn_scores.masked_fill_(mask_bool, -torch.inf)
+
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+
+        context_vecs = (attn_weights @ values).transpose(1,2)
+
+        context_vecs = context_vecs.reshape(b, num_tokens, self.d_out)
+        context_vecs = self.out_proj(context_vecs)
+
+        return context_vecs
+
 
 def precompute_rope_params(head_dim, theta_base=10_000, context_length=4096):
     assert head_dim % 2 == 0, "Embedding dimension must be even."
@@ -52,4 +108,3 @@ def compute_rope(x, cos, sin):
 
     return x_rotated.to(dtype=x.dtype)
 
-        
